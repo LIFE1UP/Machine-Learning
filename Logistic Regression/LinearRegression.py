54c85653e00cb4c9c99c033829828829cf1b3382
LIFE1UP <88600101+LIFE1UP@users.noreply.github.com>
2021-10-28 17:43:17 +0900

    add thresholder

--- Logistic Regression/LinearRegression.py
+++ Logistic Regression/LinearRegression.py
@@ -2,13 +2,14 @@ import numpy as np
 
 class linear_regression:
     def __init__(self, X, Y, LearingRate):
-        self.x = X
+        self.x = np.c_[np.ones(X.shape[0]), X]
         self.y = Y
         self.lr = LearingRate
-        self.theta = np.random.rand(X.shape[1])
+        self.theta = np.random.rand(self.x.shape[1])
 
-    def loss_function(self):
-        print(0.5 * np.sum(((np.dot(self.x.T, self.theta) - self.y) ** 2)))
+        # for predict thresholder
+
+        self.persize = (int(np.argmax(np.unique(self.y))) - int(np.argmin(np.unique(self.y)))) / int(np.unique(self.y).shape[0])
 
     def gradient(self, iteration):
         for _ in range(0, iteration):
@@ -17,14 +18,25 @@ class linear_regression:
 
     def predict(self, case):
         self.gradient(1000)
-        pr = np.dot(case, self.theta)
-        if pr >= 1.34:
-            return 2
-        elif 0.67 < pr < 1.34:
-            return 1
-        else:
-            return 0
+        self.pr = np.dot(case, self.theta)
+
+        for d in range(1, int(np.unique(self.y).shape[0])):
+            if self.pr > (self.persize * d):
+                pass
+            else:
+                return d - 1
+        return int(np.argmax(np.unique(self.y)))
+
     def predict_(self, case):
         self.gradient(1000)
-        pr = np.dot(case, self.theta)
-        return pr
+        return np.dot(case, self.theta)
+
+    def inspect(self, command):
+        if command == "Loss Function":
+            print(0.5 * np.sum(((np.dot(self.x.T, self.theta) - self.y) ** 2)))
+        elif command == "Weight":
+            print(self.theta)
+        elif command == "h(x)":
+            print(self.pr)
+        else:
+            print("Parameter Error")
--- Logistic Regression/main.py
+++ Logistic Regression/main.py
@@ -11,9 +11,10 @@ x, y = iris.data, iris.target
 first = lnreg.linear_regression(x, y, 0.0001)
 
 # matplotlib, draw a graph
-plt.plot([first.predict_(x[_]) for _ in range(0, 150)], 'b', label='h(x)')
+plt.plot([first.predict_(np.c_[np.ones(x.shape[0]), x][_]) for _ in range(0, 150)], 'g', label='h(x)')
+plt.plot([first.predict(np.c_[np.ones(x.shape[0]), x][_]) for _ in range(0, 150)], 'b', label='thr_h(x)')
+plt.plot(y, 'r--', label="Y")
 plt.xlabel('Index')
 plt.ylabel('Y')
-plt.plot(y, 'r', label="Y")
 plt.legend()
 plt.show()
