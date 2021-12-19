c4a37aa753783f1745dd7ee9eadca51303e6f705
LIFE1UP <88600101+LIFE1UP@users.noreply.github.com>
2021-12-19 14:50:45 +0900

    df

--- ADALINE/ADALINE.py
+++ ADALINE/ADALINE.py
@@ -3,93 +3,71 @@ import numpy as np
 def Counter_intf(itearation, curr):
     if curr % (itearation / 100) == 0:
         print("#", end="")
-    else:
-        pass
-#end
 
+
+# Class of Model
 class perceptron:
     def __init__(self, X, Y):
-        # creating field for machine learning
-        self.x = np.c_[np.ones(X.shape[0]), X]
+        self.x = X
         self.y = Y
         self.theta = np.random.rand(self.x.shape[1])
         self.N_1 = 1 / self.x.shape[0]
-    #end
 
-# activation/threshold functions
-    def none(self, z):
-        return z
-    #end
 
-    def step(self, z):
-        return 0 if z < 0.5 else 1
-    #end
-
+# Activation/Threshold f(x)
     def sigmoid(self, z):
         return 1.0 / (1.0 + (2.718 ** -z))
-    #end
 
     def act_choice(self, inpt, name):
-        if name == 'none':
-            return self.none(inpt)
-        elif name == 'step':
-            return self.step(inpt)
-        elif name == 'sigmoid':
+        if name == 'sigmoid':
             return self.sigmoid(inpt)
-        else:
-            print("set activation function / function function")
-    #end
 
-# Gradient Descent Rule... Choose your cost function!
-    def GDR_MSE(self, activation='noactivation'):
-        # Calulate gradient and update theta
+        return inpt
+
+
+# Gradient Descent Rule: Choose your cost function!
+    def GDR_MSE(self, activation='noneactivation'):
         hx = np.dot(self.x, self.theta)
+        hx = self.act_choice(hx, activation)
         self.theta -= (self.lr * self.N_1) * np.dot(self.x.T, (hx - self.y))
         del hx
-    #end
 
     def GDR_LogLike(self, activation='sigmoid'):
         hx = np.dot(self.x, self.theta)
-        hx = self.act_choice(hx, activation)  # activation function (sigmoid)
+        hx = self.act_choice(hx, activation)
         self.theta += self.lr * (self.N_1) * np.dot(self.x.T, (self.y - hx))
         del hx
-    #end
 
     def GDR_Cross(self,  activation='sigmoid'):
         hx = np.dot(self.x, self.theta)
         hx = self.act_choice(hx, activation)
         self.theta += self.lr * (self.N_1) * np.dot(self.x.T, (self.y - hx))
         del hx
-    #end
 
     def cost_choice(self, name_c, name_a):
-        if name_c == 'MSE':
-            self.GDR_MSE(name_a)
-        elif name_c == 'LogLike':
-            self.GDR_LogLike(name_c)
+        if name_c == 'LogLike':
+            self.GDR_LogLike(name_a)
         elif name_c == 'Cross-Entropy':
-            self.GDR_Cross(name_c)
-    #end
+            self.GDR_Cross(name_a)
 
-# options
+        return self.GDR_MSE(name_a)
+
+
+# Options
     def localeight(self, case, tau):
-        # With normalized model, we can ignore data might be useless for classification
-        low = tau ** 2 * -2
-        above = (self.x - case) ** 2
-        localized = np.exp(above / low)  # exponential to drop the data that is far from our case, tau is for setting width of our data
+        comp = ((self.x - case) ** 2) / (tau ** 2 * -2)
+        localized = np.exp(comp)
         self.x = np.multiply(self.x, localized)
 
-# predict
+
+# Predict
     def learning(self, cost, activation, iteration, lr=0.0001):
         self.lr = lr
         for iters in range(iteration):
             self.cost_choice(cost, activation)
             Counter_intf(iteration, iters)
         del self.lr
-        #end
 
     def predict(self, case, threshold):
         predicted_y = np.dot(case, self.theta)
         return self.act_choice(inpt=predicted_y, name=threshold)
-    #end
-#end
--- ADALINE/main.py
+++ ADALINE/main.py
@@ -7,27 +7,24 @@ import matplotlib.pyplot as plt
 
 # bring data
 x, y = datasets._samples_generator.make_blobs(n_samples=200, n_features=2, centers=2, cluster_std=5, random_state=11)
-# make an instance to make a machine learning
-x_ = np.c_[np.ones(x.shape[0]), x]
+x = np.c_[np.ones(x.shape[0]), x]
 
 model = ada.perceptron(x, y)
-model.learning(cost='MSE', activation='step', iteration=100000, lr=0.0001)
+model.learning(cost='MSE', activation='sigmoid', iteration=100000, lr=0.0001)
 print("")
 
 while 1:
     try:
         index = int(input("index> "))
-        case1 = x_[index, :]
-        predcition = model.predict(case1, threshold='step')
-        print(predcition, y[index])
+        case = x[index, :]
+
+        print(f"Prediction: {round(model.predict(case, threshold='sigmoid'))} Actual Target: {y[index]}")
 
         fig, ax1 = plt.subplots()
-        ax1.scatter(x[:, 0], x[:, 1], c=y)
-        ax1.scatter(x[index, 0], x[index, 1], c='b')
+        ax1.scatter(x[:, 1], x[:, 2], c=y)
+        ax1.scatter(x[index, 1], x[index, 2], c='b')
         fig.show()
         del fig, ax1
 
     except:
         continue
-    #end
-#end
